15795b0371f93ff367edc2427be62b687a745e50
Kanchan <kanchankumar.sharma@yesquesttech.com>
2022-06-08 16:06:04 +0530

    initial

--- src/main/java/com/main/MyMain.java
+++ src/main/java/com/main/MyMain.java
@@ -1,7 +1,11 @@
 package com.main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MyMain {
     public static void main(String[] args) {
-        System.out.println("hello");
+        SpringApplication.run(MyMain.class, args);
     }
 }
---
+++ src/main/java/com/main/controller/MyMainController.java
@@ -0,0 +1,13 @@
+package com.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MyMainController {
+    @RequestMapping
+    public String test(){
+        return "Success";
+    }
+}
