e90666694038a63d74740e41d9d34357a32c8c1e
Kanchan <kanchankumar.sharma@yesquesttech.com>
2022-06-08 16:20:01 +0530

    initial

--- src/main/java/com/main/controller/MyMainController.java
+++ src/main/java/com/main/controller/MyMainController.java
@@ -2,12 +2,14 @@ package com.main.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping
 public class MyMainController {
     @RequestMapping
-    public String test(){
-        return "Success";
+    public ModelAndView test(ModelAndView andView){
+        andView.setViewName("index");
+        return andView;
     }
 }
